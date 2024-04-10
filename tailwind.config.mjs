5feab721f9fae71eff51035ca7e7d6478c586fc3
Guillermo Vera Ghanain <guilleverag@gmail.com>
2024-04-10 07:38:41 -0500

    final page template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,42 +1,83 @@
 /** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
+
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        flaticon: ["Flaticon"],
         inter: ["Inter", "sans-serif"],
       },
 
       colors: {
         primary: {
-          50: "#dbe9ff",
-          100: "#c8dcfe",
-          200: "#a9caff",
-          300: "#7caefe",
-          400: "#4b8ffb",
-          500: "#3c83f6",
-          600: "#1164e8",
-          700: "#185cc9",
-          800: "#184ca0",
-          900: "#183e7b",
-          950: "#112545",
+          50: "#e8f1ff",
+          100: "#e0ecff",
+          200: "#c7dbfe",
+          300: "#a5c2fc",
+          400: "#819ff8",
+          500: "#637cf1",
+          600: "#4656e5",
+          700: "#3844ca",
+          800: "#303ba3",
+          900: "#2e3881",
+          950: "#1b1f4b",
         },
         secondary: {
-          50: "#eff1f5",
-          100: "#e4e5ec",
-          200: "#d0d6e2",
-          300: "#c4ccd9",
-          400: "#96a5bb",
-          500: "#7d8faa",
-          600: "#6c7d99",
-          700: "#60708a",
-          800: "#515e70",
-          900: "#444e5a",
-          950: "#2b3036",
+          50: "#f5f7fa",
+          100: "#e9eef5",
+          200: "#cfdae8",
+          300: "#a5bcd4",
+          400: "#7498bc",
+          500: "#537ca4",
+          600: "#406289",
+          700: "#35506f",
+          800: "#2f445d",
+          900: "#2b3b4f",
+          950: "#141b25",
+        },
+      },
+
+      animationDelay: {
+        0: "0ms",
+        200: "200ms",
+        400: "400ms",
+        600: "600ms",
+      },
+
+      animation: {
+        reveal: "reveal .6s ease-out both",
+        "slide-in-left": "slide-in-left .6s ease-out both",
+        "slide-in-right": "slide-in-right .6s ease-out .3s both",
+      },
+      keyframes: {
+        reveal: {
+          from: { opacity: 0, translate: "0 200px" },
+          to: { opacity: 1, translate: "0 0" },
+        },
+        "slide-in-left": {
+          "0%": {
+            transform: "translateX(-200px)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+        "slide-in-right": {
+          "0%": {
+            transform: "translateX(200px)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
         },
       },
     },
   },
-  plugins: [],
+  plugins: [animations],
 };
---
+++ untyped.d.ts
@@ -0,0 +1,4 @@
+declare module "wiscroll" {
+  const Wiscroll: any;
+  export = Wiscroll;
+}
